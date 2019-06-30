06dd28b80f2b1865a678406ce739251984ba56bf
APEX Knight <sgbienaime@github.com>
2019-06-30 15:07:13 -0400

    Hello Unity World working

---
+++ Assets/ChangeName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class ChangeName : MonoBehaviour
+{
+  
+    void Update()
+    {
+         if (Input.GetKeyDown("space"))
+        {
+        TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+        textmeshPro.SetText("Hello World from Sara"); 
+        }
+    }
+}
